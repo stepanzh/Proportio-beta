2d3b1e452df7288b89921d836283ada16868cf81
stepanzh <stepanzh@gmail.com>
2024-06-08 16:53:23 +0300

    feat: HelpView

--- src/views/proportio/ProportioView.vue
+++ src/views/proportio/ProportioView.vue
@@ -1,13 +1,14 @@
 <template>
     <CalculatorView v-if="proportio.currentScreen == proportio.screens.calc" />
     <EditView v-if="proportio.currentScreen == proportio.screens.edit" />
-    <div v-if="proportio.currentScreen == proportio.screens.help">Help screen</div>
+    <HelpView v-if="proportio.currentScreen == proportio.screens.help" />
 </template>
 
 
 <script setup>
 import CalculatorView from '@/views/proportio/CalculatorView.vue'
 import EditView from '@/views/proportio/EditView.vue'
+import HelpView from '@/views/proportio/HelpView.vue'
 import { useProportioNavStore } from '@/stores/proportioNav'
 
 const proportio = useProportioNavStore()
