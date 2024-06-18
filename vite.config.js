0f80365160fd2ab528b5c0d3f47961768522c588
stepanzh <stepanzh@gmail.com>
2024-06-18 16:36:35 +0300

    feat: yandex.metrica

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,28 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-  ],
+
+    VitePluginRadar({
+      metrica: [
+        {
+          id: 95640747,
+          config: {
+            defer: true,
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true,
+          }
+        },
+      ],
+    }),  // end VitePluginRadar
+  ], // end plugins
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
